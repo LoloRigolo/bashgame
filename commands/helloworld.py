6fa3d3cf1598eeaf8fa4148871747f5933bfeae6
Lorenzo <lorenzo.cesanarale@gmail.com>
2024-09-29 14:30:52 +0200

    fix(env_var): add next_days() function to handle overflow when adding too many hours; simplify function names fix: rename run_hour() to run() in command functions for consistency fix: display function description when too many parameters are provided feat: create utilities_functions.py to group common utility functions feat: add descriptions to all functions for better documentation

--- commands/helloworld.py
+++ commands/helloworld.py
@@ -1,16 +1,20 @@
-def helloworld(kwargs):
-    if len(kwargs) != 0:
-        if len(kwargs) > 1:
-            if kwargs[0] in {"-name"}:
-                helloworld_text: str = f"Hello, {kwargs[1]}!"
-        else:
-            return helloworld_description(kwargs[0])
-    else:
-        helloworld_text: str = "Hello, World!"
+# Options and description for the function
+# {"-option" : "sa description"}
+OPTIONS: dict = {"-name":"This command tell you hello !"}
+
+def run(options: list):
+    # Run the command if it's not possible call description
+    if len(options) == 0:
+         helloworld_text: str = "Hello, World!"
+    elif len(options) == 1 or len(options) >= 3:
+        return description(options[0])
+    elif (options[0] in OPTIONS):
+            if options[0] in {"-name"}:
+                helloworld_text: str = f"Hello, {options[1]}!"      
     return helloworld_text
 
-def helloworld_description(command_option) -> list:
-    OPTIONS: dict = {"-name":"This command tell you hello !"}
+def description(command_option:str = "") -> list:
+    # Show the description of the command or of this parameters's descriptions
     if command_option == "-name":
         helloworld_description_text: list= ["helloworld", "-name YOURNAME",OPTIONS[command_option]]
     else:
